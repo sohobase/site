083f56d290fe935615b3ea6209fef2c999d4d3a0
@soyjavi <javi.jimenez.villar@gmail.com>
2016-11-20 15:30:54 +0700

    Added webpack (babel + react + cssmodules)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// require('babel-register');
+// require('babel-polyfill');
+//
+// const hook = require('css-modules-require-hook');
+// hook({ generateScopedName: '[name]__[local]___[hash:base64:5]' });
+import react from 'react';
+
+
+console.log('hello world', react);
